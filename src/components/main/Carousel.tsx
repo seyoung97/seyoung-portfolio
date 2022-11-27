cf6c5abff14b44c7e3d2660f4797d4002d50e61d
Joey <seyoung1st@gmail.com>
2022-11-28 00:28:20 +0900

    ✨Feat: main page carousel에 콘텐츠 추가

--- src/components/main/Carousel.tsx
+++ src/components/main/Carousel.tsx
@@ -9,7 +9,11 @@ import { Navigation } from "swiper";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { darkGray, butter, layout } from "../../theme";
-import lululab from "../../assets/images/lululab5.png";
+import lululab from "../../assets/images/lululab6.png";
+import haii from "../../assets/images/haii.png";
+import bitbyte from "../../assets/images/bitbyte.png";
+import modu from "../../assets/images/modu.png";
+import justmeat from "../../assets/images/justmeat.png";
 
 const Carousel = () => {
   return (
@@ -18,83 +22,19 @@ const Carousel = () => {
         <img src={lululab}></img>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={lululab}></img>
+        <img src={haii}></img>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={bitbyte}></img>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={modu}></img>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={justmeat}></img>
       </SwiperSlide>
     </Swiper>
   );
 };
-
-/*const Section = styled.section`
-  width: 100%;
-  padding: 100px 0px 150px 0px;
-  color: #fffef2;
-  position: absolute;
-  top: 15%;
-  div.swiper_container {
-    display: flex;
-    align-items: center;
-    width: fit-content;
-    margin: auto;
-    .back_box {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-right: 5rem;
-      width: 80px;
-      height: 80px;
-      border: 1px solid ${butter};
-      border-radius: 50%;
-      color: ${butter};
-      .back {
-        font-size: 2rem;
-        text-align: center;
-      }
-    }
-    .next_box {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-left: 5rem;
-      width: 80px;
-      height: 80px;
-      border: 1px solid ${butter};
-      border-radius: 50%;
-      color: ${butter};
-      .next {
-        font-size: 2rem;
-        text-align: center;
-      }
-    }
-
-    div.introduction {
-      margin-right: 30px;
-      padding-top: 50px;
-      h3.title {
-        font-size: 2.5rem;
-        font-weight: bold;
-      }
-      h3.title_detail {
-        font-size: 2.3rem;
-      }
-      p {
-        width: 310px;
-        margin-top: 50px;
-        font-size: 1.1rem;
-      }
-    }
-    div.image_box {
-      
-      width: 900px;
-      height: 600px;
-      border-radius: 10px;
-      img {
-        object-fit: contain;
-        width: 100%;
-        height: 100%;
-        border-radius: 10px;
-      }
-    }
-  }
-`;*/
 
 export default Carousel;
